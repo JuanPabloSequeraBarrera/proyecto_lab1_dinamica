fc0f75f6ade86501c728cfb444afe99e7d61954a
Al3gator <mariale.cadenam@gmail.com>
2026-09-01 14:43:28 -0500

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -1,4 +1,13 @@
-
+import numpy as np
+import sympy as sp 
+from configuracion_robot import VELOCIDAD_PRUEBAS
+from control_robot import (
+    conectar_robot,
+    ejecutar_trayectoria_angulos,
+    mover_angulos_y_verificar,
+    validar_angulos, cinematica_inversa
+)
+from fase1_trayectorias import pasd
 
 def mover_desde_xy(mc, x, y, l1, l2, velocidad=30):
 
